bacb8fdfc34e891cfa4fcd8e66967318a6005512
natbates <nathaniel.christopher.bates@gmail.com>
2025-02-10 16:37:11 +0000

    stats container fix

--- src/components/sections/Statistics.jsx
+++ src/components/sections/Statistics.jsx
@@ -4,8 +4,10 @@ const StatisticsSection = () =>
 {
     return (
         <div id = "statistics" className = "home-section">
-            <h2>Numbers I'm proud of.</h2>
-            <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis.</p>
+            <div className="about-text-container">
+                <h2>Numbers I'm proud of.</h2>
+                <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis.</p>
+            </div>
             <div className="shuttleworth-stats-container">
                 <div className="stats-container">
                     <img src = "svgs/plane.svg"></img>
